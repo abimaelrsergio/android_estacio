a386e45e37361cb74cce207a5b4ccddce6beb8ca
Abimael Rodrigues Sergio <abimaelr.sergio@gmail.com>
2020-09-16 20:31:25 -0300

    Primeiro commmit

--- app/src/main/java/br/com/fucionario/iu/activity/FormularioFuncionarioActivity.java
+++ app/src/main/java/br/com/fucionario/iu/activity/FormularioFuncionarioActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import br.com.fucionario.R;
@@ -15,13 +16,18 @@ public class FormularioFuncionarioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_funcionario);
+
+        final EditText campoNome = findViewById(R.id.activity_formulario_funcionario_nome);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_funcionario_telefone);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_funcionario_email);
         Button botaoSalvar = findViewById(R.id.activity_formulario_funcionario_botao_salvar);
+
         botaoSalvar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Toast.makeText(FormularioFuncionarioActivity.this,
-                        "Clicaram no botão",
-                        Toast.LENGTH_LONG).show();
+                String nome = campoNome.getText().toString();
+                String telefone = campoTelefone.getText().toString();
+                String email = campoEmail.getText().toString();
             }
         });
     }
---
+++ app/src/main/java/br/com/fucionario/model/Funcionario.java
@@ -0,0 +1,30 @@
+package br.com.fucionario.model;
+
+public class Funcionario {
+
+    private final String nome;
+    private final String telefone;
+    private final String email;
+
+    private Funcionario(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String toString() {
+        return nome;
+    }
+}
