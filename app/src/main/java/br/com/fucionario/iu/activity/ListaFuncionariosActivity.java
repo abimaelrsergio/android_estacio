caa6f6c7d0fc2b19e58de754ae932f819b9376ee
Abimael Rodrigues Sergio <abimaelr.sergio@gmail.com>
2020-09-16 21:26:46 -0300

    Primeiro commmit

--- app/src/main/java/br/com/fucionario/iu/activity/ListaFuncionariosActivity.java
+++ app/src/main/java/br/com/fucionario/iu/activity/ListaFuncionariosActivity.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import br.com.fucionario.R;
+import br.com.fucionario.dao.FuncionarioDAO;
 
 public class ListaFuncionariosActivity extends AppCompatActivity {
 
@@ -19,12 +20,14 @@ public class ListaFuncionariosActivity extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_funcionarios);
+
+        FuncionarioDAO funcionarioDao = new FuncionarioDAO();
+
         setTitle("Lista Funcionarios");
-        List<String> funcionarios = new ArrayList<>(
-                Arrays.asList("Andre","Antonio","Maria","Ana","Joao","Pedro","Daniel","Daniela","Wagner","Leandro","Abimael","Gisela","Gisele","Gislene","Joana","Maria Joaquina","Quirino","Sirino","Chaves","Quico","Kiko","Quico"));
         ListView listaDeFuncionarios = findViewById(R.id.activity_lista_funcionarios);
         listaDeFuncionarios.setAdapter(
-                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, funcionarios));
+                new ArrayAdapter<>(this,
+                             android.R.layout.simple_list_item_1, funcionarioDao.getAll()));
 
     }
 }
